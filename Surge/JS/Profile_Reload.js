6605dff09952a9afbf7b3641bcee8d8093219c42
MrCatLord <105979538+MrCatLord@users.noreply.github.com>
2022-05-28 01:09:43 +0800

    Add files via upload

---
+++ Surge/JS/Profile_Reload.js
@@ -0,0 +1,18 @@
+$httpAPI("POST", "/v1/profiles/reload", {}, data => {
+   var now = new Date();
+   var month = now.getMonth();
+   var date = now.getDate();
+   var hour = now.getHours();
+   var minutes = now.getMinutes();
+   var seconds = now.getSeconds();
+   var monthArray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+   hour = hour > 9 ? hour : "0" + hour;
+   minutes = minutes > 9 ? minutes : "0" + minutes;
+   seconds = seconds > 9 ? seconds : "0" + seconds;
+    $done({
+        title:"Profile Reload",
+        content:"Last runtime:  "+monthArray[month]+" "+date+"  "+hour+":"+minutes+":"+seconds,
+        icon:"circle.dashed.inset.filled",
+        "icon-color": "#FF9500",
+     })
+    }); 
