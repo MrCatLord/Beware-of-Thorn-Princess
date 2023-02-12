2faffc281fd70b7c61f3c28a44dd8f6afc662b4c
MrCatLord <105979538+MrCatLord@users.noreply.github.com>
2023-02-12 12:13:17 +0800

    更新 SurgePro.js

--- Surge/JS/SurgePro.js
+++ Surge/JS/SurgePro.js
@@ -10,7 +10,6 @@ let startTime = timeTransform(dateNow,dateTime)
 if ($trigger == "button") await httpAPI("/v1/profiles/reload");
 
   $done({
-      title:"𝑺𝒖𝒓𝒈𝒆  𝑷𝒓𝒐",
       content:`启动时长: ${startTime}`,
 		icon: params.icon,
 		"icon-color":params.color
