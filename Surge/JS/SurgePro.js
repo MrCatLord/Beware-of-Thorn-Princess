a796e689580e6fe42ff51119378b2e64a89db9b0
MrCatLord <105979538+MrCatLord@users.noreply.github.com>
2023-02-14 00:34:45 +0800

    更新 SurgePro.js

--- Surge/JS/SurgePro.js
+++ Surge/JS/SurgePro.js
@@ -16,7 +16,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"𝑺𝒖𝒓𝒈𝒆 𝑷𝒓𝒐  运行"+startTime,     
+    title:"𝑺𝒖𝒓𝒈𝒆 𝑷𝒓𝒐 运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"exclamationmark.triangle",
    "icon-color":icon_s?"#f6c970":"#FF7500"
